e242e091c93482ad59b9bd1e71550031907361fc
Lufx2ytaro <kunticha09@gmail.com>
2025-09-03 19:06:14 +0300

    understand how work @ write simple square calculator

--- obj/Debug/net9.0/Blazor.AssemblyInfo.cs
+++ obj/Debug/net9.0/Blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcdd0aecc9c854db3a0793c20b963131b740d3a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+240cecb43519553c1ec4f4a5944a54f5ecbf8f6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
