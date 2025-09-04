d1d9de1a30109beffa507be926e80990374611c2
Lufx2ytaro <kunticha09@gmail.com>
2025-09-04 17:51:43 +0300

    Буду переделывать калькулятор + добавил рассчёт электро с возможностью выбора городов

--- obj/Debug/net9.0/Blazor.AssemblyInfo.cs
+++ obj/Debug/net9.0/Blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e242e091c93482ad59b9bd1e71550031907361fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf895ff5a7be48ab66a8cb79c48b3cad466f972b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
